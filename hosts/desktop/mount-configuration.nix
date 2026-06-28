7b2c8dbce5689b8a1c3cecf0b8e4d8c1ad8b5fa7
nayetdet <jpmoreiracampos21@gmail.com>
2026-06-28 18:34:42 -0300

    fix(steam): fixed desktop mount configuration to make steam be able to access the external drives

--- hosts/desktop/mount-configuration.nix
+++ hosts/desktop/mount-configuration.nix
@@ -3,11 +3,13 @@
     device = "/dev/disk/by-uuid/85aa32eb-672c-423d-9428-57d7e28d0525";
     fsType = "ext4";
     options = [
+      "rw"
+      "exec"
       "discard"
-      "users"
       "nofail"
       "noatime"
       "x-systemd.automount"
+      "x-systemd.idle-timeout=60"
     ];
   };
 
@@ -15,11 +17,13 @@
     device = "/dev/disk/by-uuid/34063fba-ef9a-4f0a-927c-022222f5696f";
     fsType = "ext4";
     options = [
+      "rw"
+      "exec"
       "discard"
-      "users"
       "nofail"
       "noatime"
       "x-systemd.automount"
+      "x-systemd.idle-timeout=60"
     ];
   };
 }
