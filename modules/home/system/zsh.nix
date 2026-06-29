98909e6e745cadd47b137b2614ff781fbbf5878f
nayetdet <jpmoreiracampos21@gmail.com>
2026-06-29 09:41:02 -0300

    feat(zsh): added nix agents shell in zsh aliases

--- modules/home/system/zsh.nix
+++ modules/home/system/zsh.nix
@@ -1,17 +1,30 @@
-{ ...}: {
-  programs.zsh = {
-    enable = true;
-    enableCompletion = true;
-    autosuggestion.enable = true;
-    syntaxHighlighting.enable = true;
-    oh-my-zsh = {
+{ self, config, lib, ... }: {
+  options.zsh.aliases.enable = lib.mkOption {
+    type = lib.types.bool;
+    default = true;
+    description = "enable zsh aliases";
+  };
+
+  config = {
+    programs.zsh = {
       enable = true;
-      theme = "gentoo";
-      plugins = [
-        "git"
-        "sudo"
-        "zoxide"
-      ];
+      enableCompletion = true;
+      autosuggestion.enable = true;
+      syntaxHighlighting.enable = true;
+      oh-my-zsh = {
+        enable = true;
+        theme = "gentoo";
+        plugins = [
+          "git"
+          "sudo"
+          "zoxide"
+        ];
+      };
+
+      shellAliases = lib.mkIf config.zsh.aliases.enable {
+        codex = "nix develop ${self}#agents -c codex";
+        opencode = "nix develop ${self}#agents -c opencode";
+      };
     };
   };
 }
