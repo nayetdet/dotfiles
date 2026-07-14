b3d811ffd7e109639bb073a6bff0d717b5576810
nayetdet <jpmoreiracampos21@gmail.com>
2026-07-14 11:51:23 -0300

    feat(flameshot): update configuration and improve screenshot handling

--- modules/home/flatpak/flameshot.nix
+++ modules/home/flatpak/flameshot.nix
@@ -1,22 +1,53 @@
-{ self, config, lib, pkgs, ... }:
+{ config, pkgs, ... }:
 let
-  screenshotsDir = "${config.home.homeDirectory}/Pictures/Screenshots";
-  flameshotConfigSource = self + /config/flameshot/flameshot.ini;
-  flameshotConfigTarget = "${config.home.homeDirectory}/.var/app/org.flameshot.Flameshot/config/flameshot/flameshot.ini";
-in {
-  home.packages = with pkgs; [
-    wl-clipboard
-  ];
+  flameshotScreenshotsDirectory = "${config.home.homeDirectory}/Pictures/Screenshots";
+  flameshotIniPath = "${config.home.homeDirectory}/.var/app/org.flameshot.Flameshot/config/flameshot/flameshot.ini";
+  flameshotIni = (pkgs.formats.ini {}).generate "flameshot.ini" {
+    General = {
+      contrastOpacity = 188;
+      saveAfterCopy = true;
+      savePath = flameshotScreenshotsDirectory;
+      showStartupLaunchMessage = false;
+    };
 
+    Shortcuts = {
+      TYPE_ACCEPT = "Ctrl+C";
+    };
+  };
+in {
   services.flatpak.packages = [
     "org.flameshot.Flameshot"
   ];
 
-  home.activation.flameshotSetup = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-    mkdir -p "${screenshotsDir}"
-    install -Dm644 "${flameshotConfigSource}" "${flameshotConfigTarget}"
-    ${pkgs.flatpak}/bin/flatpak permission-set screenshot screenshot org.flameshot.Flameshot yes
-  '';
+  home.packages = with pkgs; [
+    wl-clipboard
+  ];
+
+  systemd.user.tmpfiles.rules = [
+    "d ${flameshotScreenshotsDirectory} 0755 - - -"
+    "C ${flameshotIniPath} - - - - ${flameshotIni}"
+  ];
+
+  systemd.user.services.flameshotScreenshotPermission = {
+    Unit = {
+      Description = "Flameshot screenshot portal permission";
+      Wants = [ "flatpak-managed-install.service" ];
+      After = [
+        "flatpak-managed-install.service"
+        "graphical-session.target"
+      ];
+    };
+
+    Service = {
+      Type = "oneshot";
+      ExecCondition = "${pkgs.flatpak}/bin/flatpak info org.flameshot.Flameshot";
+      ExecStart = "${pkgs.flatpak}/bin/flatpak permission-set screenshot screenshot org.flameshot.Flameshot yes";
+    };
+
+    Install = {
+      WantedBy = [ "graphical-session.target" ];
+    };
+  };
 
   dconf.settings = {
     "org/gnome/shell/keybindings" = {
@@ -31,7 +62,7 @@ in {
 
     "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1" = {
       binding = "Print";
-      command = ''bash -c "QT_QPA_PLATFORM=wayland flatpak run --command=flameshot org.flameshot.Flameshot gui -p "${screenshotsDir}" -r | wl-copy"'';
+      command = ''${pkgs.bash}/bin/bash -c "QT_QPA_PLATFORM=wayland ${pkgs.flatpak}/bin/flatpak run --command=flameshot org.flameshot.Flameshot gui -p "${flameshotScreenshotsDirectory}" -r | ${pkgs.wl-clipboard}/bin/wl-copy"'';
       name = "Flameshot";
     };
   };
