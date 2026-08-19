1d9d65d86355f6434509499df5dd57a78ce2cabb
nayetdet <jpmoreiracampos21@gmail.com>
2026-08-19 12:36:21 -0300

    fix(nixos): update udev rules for Kinect USB devices to set appropriate permissions

--- modules/nix/system/kinect.nix
+++ modules/nix/system/kinect.nix
@@ -2,8 +2,8 @@
   boot.blacklistedKernelModules = [ "gspca_kinect" ];
 
   services.udev.extraRules = ''
-    SUBSYSTEM=="usb", ATTR{idVendor}=="045e", ATTR{idProduct}=="02ae", TAG+="uaccess"
-    SUBSYSTEM=="usb", ATTR{idVendor}=="045e", ATTR{idProduct}=="02ad", TAG+="uaccess"
-    SUBSYSTEM=="usb", ATTR{idVendor}=="045e", ATTR{idProduct}=="02c2", TAG+="uaccess"
+    SUBSYSTEM=="usb", ATTR{idVendor}=="045e", ATTR{idProduct}=="02ae", MODE="0660", GROUP="users", TAG+="uaccess"
+    SUBSYSTEM=="usb", ATTR{idVendor}=="045e", ATTR{idProduct}=="02ad", MODE="0660", GROUP="users", TAG+="uaccess"
+    SUBSYSTEM=="usb", ATTR{idVendor}=="045e", ATTR{idProduct}=="02c2", MODE="0660", GROUP="users", TAG+="uaccess"
   '';
 }
