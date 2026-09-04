9f9ba1e082b5552b49784f96b4f86eaa9f00a66b
nayetdet <jpmoreiracampos21@gmail.com>
2026-09-03 21:44:49 -0300

    feat(nixos): added avatars in gnome

--- modules/nix/system/gnome.nix
+++ modules/nix/system/gnome.nix
@@ -1,9 +1,20 @@
-{ pkgs, ... }: {
+{ self, host, pkgs, ... }:
+let
+  accountsServiceUser = pkgs.writeText "accountsservice-${host.user.name}" ''
+    [User]
+    Icon=/var/lib/AccountsService/icons/${host.user.name}
+  '';
+in {
   services = {
     displayManager.gdm.enable = true;
     desktopManager.gnome.enable = true;
     gnome.core-apps.enable = false;
   };
+
+  systemd.tmpfiles.rules = [
+    "L+ /var/lib/AccountsService/icons/${host.user.name} - - - - ${self}/assets/avatars/${host.id}.png"
+    "L+ /var/lib/AccountsService/users/${host.user.name} - - - - ${accountsServiceUser}"
+  ];
 
   environment.gnome.excludePackages = with pkgs; [
     gnome-tour
