bad8e7a174b42a4b9931c60709bcf8a691fda018
nayetdet <jpmoreiracampos21@gmail.com>
2026-09-03 22:13:39 -0300

    feat(nixos): update accounts service paths and add read-write configuration for accounts-daemon

--- modules/nix/system/gnome.nix
+++ modules/nix/system/gnome.nix
@@ -1,20 +1,9 @@
-{ self, host, pkgs, ... }:
-let
-  accountsServiceUser = pkgs.writeText "accountsservice-${host.user.name}" ''
-    [User]
-    Icon=/var/lib/AccountsService/icons/${host.user.name}
-  '';
-in {
+{ pkgs, ... }: {
   services = {
     displayManager.gdm.enable = true;
     desktopManager.gnome.enable = true;
     gnome.core-apps.enable = false;
   };
-
-  systemd.tmpfiles.rules = [
-    "L+ /var/lib/AccountsService/icons/${host.user.name} - - - - ${self}/assets/avatars/${host.id}.png"
-    "L+ /var/lib/AccountsService/users/${host.user.name} - - - - ${accountsServiceUser}"
-  ];
 
   environment.gnome.excludePackages = with pkgs; [
     gnome-tour
